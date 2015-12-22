1e68015cec1f9f2ca6e111e3e69be4a5a42929ab
mankiw <444024676@qq.com>
2015-12-22 15:56:18 +0000

    add player

--- _rel/else_release/lib/else-rolling/src/else_app.erl
+++ _rel/else_release/lib/else-rolling/src/else_app.erl
@@ -5,6 +5,7 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    %ok = application:start(ranch),
 	else_sup:start_link().
 
 stop(_State) ->
--- src/else_sup.erl
+++ src/else_sup.erl
@@ -8,5 +8,10 @@ start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
+	{ok, _} = ranch:start_listener(tcp_echo, 100,
+		ranch_tcp, [{port, 5555}],
+		player_protocol, []
+	),
+	io:format("start ranch ok ", []),
 	Procs = [],
 	{ok, {{one_for_one, 1, 5}, Procs}}.
